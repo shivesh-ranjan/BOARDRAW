dd8c8c6f1a8fb514a85c7a2dc0319729acb774f4
Vaibhav Upreti <85568177+VaibhavUpreti@users.noreply.github.com>
2021-12-12 14:58:54 +0530

    Add files via upload

---
+++ board/client-data/js/minitpl.js
@@ -0,0 +1,45 @@
+/**
+ *                        MINITPL
+ *********************************************************
+ * @licstart  T
+ * @licend
+ */
+
+Minitpl = (function () {
+
+	function Minitpl(elem, data) {
+		this.elem = (typeof (elem) === "string") ? document.querySelector(elem) : elem;
+		if (!elem) {
+			throw "Invalid element!";
+		}
+		this.parent = this.elem.parentNode;
+		this.parent.removeChild(this.elem);
+	}
+
+	function transform(element, transformer) {
+		if (typeof (transformer) === "function") {
+			transformer(element);
+		} else {
+			element.textContent = transformer;
+		}
+	}
+
+	Minitpl.prototype.add = function (data) {
+		var newElem = this.elem.cloneNode(true);
+		if (typeof (data) === "object") {
+			for (var key in data) {
+				var matches = newElem.querySelectorAll(key);
+				for (var i = 0; i < matches.length; i++) {
+					transform(matches[i], data[key]);
+				}
+			}
+		} else {
+			transform(newElem, data);
+		}
+		this.parent.appendChild(newElem);
+		return newElem;
+	}
+
+	return Minitpl;
+}());
+
